f00e934cb46055039a9bd9b01f95eefcb27984e8
yoni <sdy32@naver.com>
2022-02-16 11:08:14 +0900

    네이버 랭킹 영화 크롤링

--- movie_crawling.py
+++ movie_crawling.py
@@ -2,13 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# data를 받아올 사이트 주소
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-# 코딩 시작
+# BeautifulSoup의 select 함수로 tr들 불러오기
 trs = soup.select('#old_content > table > tbody > tr')
 
+# 반복문을 통해 tr 각각 호출
 for tr in trs:
     a_tag = tr.select_one('td.title > div > a')
     point_tag = tr.select_one('td.point')
