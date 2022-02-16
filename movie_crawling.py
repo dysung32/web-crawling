dd4f2e5dc2d840b341b7dab607697a59372433e1
yoni <sdy32@naver.com>
2022-02-16 16:24:49 +0900

    크롤링으로 받아온 데이터 pymongo로 DB에 저장

--- movie_crawling.py
+++ movie_crawling.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017) # mongoDB에 접속
+db = client.dbsparta
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 # data를 받아올 사이트 주소
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
@@ -18,4 +22,10 @@ for tr in trs:
         rank = tr.select_one('td:nth-child(1) > img')['alt']
         title = a_tag.text
         point = point_tag.text
-        print(rank, title, point)
+        doc = {
+            'rank': rank,
+            'title': title,
+            'point': point
+        }
+        # db.movies.insert_one(doc) # movies라는 DB Collection에 데이터 삽입
+
